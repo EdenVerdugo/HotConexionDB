4fd3d9a984f8ad9156a0187dd2785fd50440c52c
eden.verdugo@gmail.com <eden.verdugo@gmail.com>
2019-05-28 18:23:02 -0600

    Main functionality completed! :)

---
+++ src/HotConexionDB/ConexionColumnAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HotConexionDB
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class ConexionColumnAttribute : Attribute
+    {
+        public string Name { get; set; }
+
+        public ConexionColumnAttribute(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
